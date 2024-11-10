d224b74a4d881737915e6d4055393286101cdf78
Sooriyamathy <47821014+Sooriyamathy@users.noreply.github.com>
2024-11-09 20:42:23 -0500

    Added logic to add student
    added primary logic to add student in the program and wrote a initialization function which initiates the whole program.

---
+++ schoolstruct.h
@@ -0,0 +1,21 @@
+#ifndef SCHOOLSTRUCT_H
+#define SCHOOLSTRUCT_H
+typedef struct student_struct
+{
+    char student_name[30];
+    int student_id;
+}student;
+typedef struct course_struct
+{
+    char course_name[30];
+    float average_grade;
+    student* enrolled_list;
+    int student_count;
+}course;
+typedef struct school_struct
+{
+    char school_name[50];
+    int num_courses;
+    course* courses_offered;
+}school;
+#endif
